be0d5dd82f042e3873af70197ceec14de556ede2
Tosh <tomerosh@gmail.com>
2026-01-22 09:55:17 +0200

    Added meta tags

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,7 @@
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import Main from '../components/Main';
-import Nav from '../components/Nav';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
