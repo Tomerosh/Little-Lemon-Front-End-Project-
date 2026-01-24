688162b98e33df6a4442b83017067a793eb82559
Tosh <tomerosh@gmail.com>
2026-01-24 13:55:17 +0200

    Navbar and Hero initiated

--- react-app/src/components/Main.js
+++ react-app/src/components/Main.js
@@ -1,6 +1,18 @@
-
+//import foodImg from "../assets/images/food-tray.jpg"
 export default function Main() {
-    return <main>
-        
+    return <main className="container">
+        <article className="hero-section">
+            <div>
+                <h1>Little Lemon</h1>
+                <h2>Chicago</h2>
+                <p>Taste the Mediterranean by the sea—fresh ingredients, vibrant flavors, and warm hospitality, inviting you to relax and savor every moment.</p>
+                <button title="Reserve a Table"></button>
+            </div>
+            <div id="img"/>
+        </article>
+        <div className="highlights">
+            <h2>Our Specials</h2>
+            <div className="card"></div>
+        </div>
     </main>
 }
--- react-app/src/components/Nav.js
+++ react-app/src/components/Nav.js
@@ -1,6 +1,17 @@
+import logo from '../assets/images/Logo.svg'
+import Basket from '../assets/images/Basket.svg'
 
 export default function Nav() {
     return <nav>
-        
+        <img id='logo' src={logo}></img>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order Online</li>
+            <li>Login</li>
+        </ul>
+        <img className='basket' src={Basket}></img>
     </nav>
 }
